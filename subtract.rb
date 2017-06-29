86d3c92cd39a9ee4c741bc6df3df641d219d02ca
stephinwv <thekingdomseeker@aol.com>
2017-06-29 14:00:51 -0400

    Add files via upload

---
+++ subtract.rb
@@ -0,0 +1,7 @@
+puts "Please enter number 1 : "
+# input in console
+num_1 = gets.chomp.to_i
+print "Please enter number 2 : "
+num_2 = gets.chomp.to_i
+num_3 = num_1 - num_2 
+print "Answer : " , (num_1 - num_2), "\n"
